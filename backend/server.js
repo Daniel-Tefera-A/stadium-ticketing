6fdb203860f34af8a2c9e00c6ec426ce4c129372
Daniel Tefera <124468496+Daniel-Tefera-A@users.noreply.github.com>
2026-03-05 20:56:13 +0300

    fix for server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173","https://stadium-ticketing.vercel.app"
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -20,7 +20,7 @@ const PORT = 3000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173", "https://stadium-ticketing.vercel.app"
   credentials: true
 }));
 app.use(express.json());
